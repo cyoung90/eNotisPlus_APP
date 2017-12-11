1c817392071f0e62fc50aee57106bc930b7efaac
ccy <chlckdud78@gmail.com>
2017-12-11 15:20:30 +0900

    init

---
+++ scripts/app.js
@@ -0,0 +1,221 @@
+(function() {
+	// ECMAScript5부터 적용되는 키워드. 쉽게말해 안전한 코딩을 위한 가이드라인
+	// 안 좋은 자바스크립트 코딩패턴들을 코칭해주는 자바스크립트 문법 검사기
+	// 최상위에 사용, IE10 이전 버전 미지원
+	'use strict';
+
+	var app = {
+//		isLoading: true,
+//		visibleCards: {},
+//		selectedCities: [],
+//		spinner: document.querySelector('.loader'),
+//		cardTemplate: document.querySelector('.cardTemplate'),
+//		container: document.querySelector('.main'),
+//		addDialog: document.querySelector('.dialog-container'),
+		autoLogin: false,
+		user: {"id":"32089829","mail":"chayoung_choi@daekyo.co.kr"},
+		daysOfWeek: ['월', '화', '수', '목', '금', '토', '일']
+	};
+
+
+/*******************************************************************************
+ * 
+ * Event listeners for UI elements
+ * 
+ ******************************************************************************/
+	$(document).ready(function(){
+		
+		console.log("# localStorage.autoLogin >>>>>>>>>>>>>>>>>>>>>>>>", localStorage);
+		
+		// 자동로그인
+		if ( localStorage.autoLogin == 'true' ){
+			var user = JSON.parse( localStorage.user );
+			$("[for='chk_autoLogin']").addClass("is-checked");
+			console.log("# 1 >>>>>>>>>>>>>>>>>>>>>>>>", user);
+			
+			if ( !gfn_isNull(user.ID) ) {
+				console.log("# 2 >>>>>>>>>>>>>>>>>>>>>>>>", user.ID);
+				$("#id").val( user.ID );
+				
+				localStorage.loginInit = "auto";
+				var comSubmit = new ComSubmit();
+				comSubmit.setUrl("/components/home/home.jsp");
+				comSubmit.submit();
+			}
+		}
+	});
+	
+	document.getElementById('btn_login').addEventListener('click', function() {
+		
+		// test 사번
+		if ( $("#id").val() == 'e' ) {
+			$("#id").val("8300120");
+			$("#pw").val("init1234");
+		}
+		
+		if ( gfn_isNull( $("#id").val().trim() )){
+			alert("아이디가 입력되지 않았습니다.");
+			$("#id").focus();
+			return false;
+		}
+		
+		if ( gfn_isNull( $("#pw").val().trim() )){
+			alert("비밀번호가 입력되지 않았습니다.");
+			$("#pw").focus();
+			return false;
+		}
+		
+		var id = document.getElementById('id').value;
+		var pw = document.getElementById('pw').value;
+		
+		app.loginProcess(id, pw);
+		
+	});
+	
+	
+	
+	
+	
+	
+
+
+/*******************************************************************************
+ * 
+ * Methods to update/refresh the UI
+ * 
+ ******************************************************************************/
+	app.saveUserInfo = function(data) {
+		
+		var lastUpdateDt = new Date();
+		data.LAST_LOGIN_DT = lastUpdateDt;
+		
+		var user = JSON.stringify(data);
+		var autoLogin = document.getElementById("chk_autoLogin").checked;
+		
+		// 로컬에 저장
+	    localStorage.user = user;	
+	    localStorage.autoLogin = autoLogin;
+	}
+
+/*******************************************************************************
+ * 
+ * Methods for dealing with the model
+ * 
+ ******************************************************************************/
+	// 로그인
+	app.loginProcess = function(id, pw) {
+		
+		var host = window.location.host;
+		
+		var url = 'http://' + host + '/main/loginProcess.do?id=' + id + '&pw=' + pw;
+		
+		if ( 'caches' in window ) {
+			caches.match(url).then(function(response) {
+				if (response) {
+					response.json().then(function updateFromCache(json) {
+						var results = json;
+						var msg = results.msg;
+						var resultCd = results.resultCd;
+						
+						console.log(results);
+						if (resultCd == "200") {
+							var userInfo = results.userInfo;
+							app.saveUserInfo(userInfo);
+							localStorage.loginInit = "init";
+							app.moveComponent("/components/home/home.jsp");
+						}
+						
+						console.log("caches");
+					});
+				}
+			});
+		}
+		// Fetch the latest data.
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function() {
+			if (request.readyState === XMLHttpRequest.DONE) {
+				if (request.status === 200) {
+					var response = JSON.parse(request.response);
+					
+					var results = response;
+					var msg = results.msg;
+					var resultCd = results.resultCd;
+					
+					if (resultCd == "200") {
+						var userInfo = results.userInfo;
+						app.saveUserInfo(userInfo);
+						localStorage.loginInit = "init";
+						app.moveComponent("/components/home/home.jsp");
+					} else {
+						alert(msg);
+					}
+					
+					console.log("XMLHttpRequest");
+				}
+			} else {
+				// Return the initial weather forecast since no data is available.
+				//app.updateForecastCard(initialWeatherForecast);
+			}
+		};
+//		var comSubmit = new ComSubmit();
+//		comSubmit.addParam("id", $("#id").val());
+//		comSubmit.addParam("pw", $("#pw").val());
+//		
+//		var formData = $("#commonForm").serialize();
+//		console.log(formData);
+		
+		request.open('GET', url);
+		request.send();
+//		request.open('POST', 'http://' + host + '/main/loginProcess.do');
+//		request.send(formData);
+	};
+	
+	app.updateForecasts = function() {
+		var keys = Object.keys(app.visibleCards);
+			keys.forEach(function(key) {
+			app.getForecast(key);
+		});
+	};
+
+	var initUser = {
+			NFUID: '8300120',
+			NAME: '최차영',
+			MAIL: 'chayoung_choi@daekyo.co.kr',
+			LAST_LOGIN_DT: '20171107180000'
+	}
+
+	// 화면 이동
+	app.moveComponent = function(url){
+		var comSubmit = new ComSubmit();
+		comSubmit.setUrl(url);
+		comSubmit.submit();
+	}
+/*******************************************************************************
+ * 
+ * Code required to start the app
+ * 
+ * NOTE: To simplify this codelab, we've used localStorage. localStorage is a
+ * synchronous API and has serious performance implications. It should not be
+ * used in production applications! Instead, check out IDB
+ * (https://www.npmjs.com/package/idb) or SimpleDB
+ * (https://gist.github.com/inexorabletash/c8069c042b734519680c)
+ ******************************************************************************/
+	app.user = localStorage.user;
+	app.autoLogin = localStorage.autoLogin;
+	if (app.user) {
+	    app.user = JSON.parse(app.user);
+	} else {
+		
+	}
+
+	// TODO add service worker code here
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker
+				 .register('./service-worker.js')
+				 .then(function(registration) { 
+					 	console.log('ServiceWorker registration successful with scope:',  registration.scope);
+				 }).catch(function(error) {
+						console.log('ServiceWorker registration failed:', error);
+				 });
+	}
+})();
---
+++ service-worker.js
@@ -0,0 +1,82 @@
+var dataCacheName = 'eNotis+_Data-v1';
+var cacheName = 'eNotis+_step_1_1';
+// 캐시 저장소
+var filesToCache = [
+	'/',
+	'/index.html',
+	'/scripts/app.js',
+	'/styles/base.css',
+	'/styles/inline.css',
+	'/include/header.html',
+	'/include/leftMenu.html',
+	'/include/include-body.jsp',
+	'/common/taglibs.jsp',
+
+	//componets
+	'/components/home/home.jsp',
+	
+	'/components/mail/writeMail.jsp',
+	'/components/home/homeApp.js'		// , 조심!!
+	/*	
+	'/components/home/homeApp.js'*/
+	];
+
+// service-worker install evnet
+self.addEventListener('install', function(e) {
+	console.log('[ServiceWorker] Install');
+	e.waitUntil(caches.open(cacheName).then(function(cache) {
+		console.log('[ServiceWorker] Caching app shell', cacheName);
+		return cache.addAll(filesToCache);
+	}));
+});
+
+// service-worker fetch event - 1
+// PWA에서 생성된 요청을 가로채서 서비스워커에서 처리하는 기능
+//self.addEventListener('fetch', function(e) {
+//	console.log('[ServiceWorker] Fetch', e.request.url);
+//	e.respondWith(caches.match(e.request).then(function(response) {
+//		return response || fetch(e.request);
+//	}));
+//});
+
+// service-worker fetch event - test
+self.addEventListener('fetch', function(e) {
+	console.log('[ServiceWorker] Fetch', e.request.url);
+	
+	var dataUrl = 'localhost';
+	if (e.request.url.indexOf(dataUrl) > -1) {
+		e.respondWith(
+		  caches.open(dataCacheName).then(function(cache) {
+		    return fetch(e.request).then(function(response){
+		      cache.put(e.request.url, response.clone());
+		      return response;
+		    });
+		  })
+		);
+	} else {
+		e.respondWith(caches.match(e.request).then(function(response) {
+			return response || fetch(e.request);
+		}));
+	}
+	
+});
+
+// service-worker activate event (Cache update)
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+
+// 사용자 앱 설치
+
