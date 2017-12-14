cec4580f0a4ff912fde2d2964989adc6ea9d7cca
ccy <chlckdud78@gmail.com>
2017-12-14 10:11:41 +0900

    d

--- js/common.js
+++ js/common.js
@@ -10,30 +10,6 @@ function gfn_isNull(str) {
 }
  
 function ComSubmit(opt_formId) {
-    this.formId = gfn_isNull(opt_formId) == true ? "commonForm" : opt_formId;
-    this.url = "";
-    
-    if(this.formId == "commonForm"){
-        $("#commonForm")[0].reset();
-    }
-     
-    this.setUrl = function setUrl(url){
-        this.url = url;
-    };
-     
-    this.addParam = function addParam(key, value){
-        $("#"+this.formId).append($("<input type='hidden' name='"+key+"' id='"+key+"' value='"+value+"' >"));
-    };
-     
-    this.submit = function submit(){
-        var frm = $("#"+this.formId)[0];
-        frm.action = this.url;
-        frm.method = "POST";
-		frm.submit();
-	};
-}
-
-function ComSubmit(opt_formId) {
 	this.formId = gfn_isNull(opt_formId) == true ? "commonForm" : opt_formId;
 	this.url = "";
 	this.method = "POST";	//default : POST
